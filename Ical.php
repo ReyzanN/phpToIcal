0a89e0af06faaa1666ac59c4d7db1828da292ff0
Reyzan - Gaétan <g.sicardchamoret@laposte.net>
2023-01-20 09:52:52 +0100

    Update add event and ical generation

--- Ical.php
+++ Ical.php
@@ -2,13 +2,13 @@
 
 class Ical
 {
-    private $name;
-    private $pathIcalSave;
-    private $proID;
-    private $version;
-    private $calscale = 'GREGORIAN';
-    private $method;
-    private $Event = array();
+    private string $name;
+    private string $pathIcalSave;
+    private string $proID;
+    private string $version;
+    private string $calscale = 'GREGORIAN';
+    private string $method;
+    private array $Event = array();
 
     /**
      * @param array $Data - ['patchIcal'] = Path to Ical | ['proID'][0] = Company Name | ['proID'][1] == Product name | ['Version'] = Version | ['Method'] = Method | ['Name'] = file name
@@ -45,6 +45,7 @@ class Ical
      */
 
     /**
+     * @usage Generate proID
      * @param array $Data - [0] Company Name - [1] Product Name
      * @return void
      */
@@ -52,6 +53,10 @@ class Ical
         return '-//'.$Data[0].'//NONSGML '.$Data[1].'//FR';
     }
 
+    /**
+     * @usage Create Ical file
+     * @return void
+     */
     private function createFile() :void{
         if (!file_exists($this->pathIcalSave.'/'.$this->name.'.ics')) {
             touch($this->pathIcalSave.'/'.$this->name.'.ics');
@@ -61,7 +66,11 @@ class Ical
         }
     }
 
-    public function writefile() :void{
+    /**
+     * @usage Write vital part of ical file
+     * @return void
+     */
+    private function writefile() :void{
         if (file_exists($this->pathIcalSave.'/'.$this->name.'.ics')){
             $file = fopen($this->pathIcalSave.'/'.$this->name.'.ics','w') or die('Problem');
             /*
@@ -75,6 +84,35 @@ class Ical
         }
     }
 
+    /**
+     * @usage Add Event for ical
+     * @param array $Data
+     * @return bool
+     */
+    public function addEvent(array $Data) :bool{
+        $this->Event[] = new EventIcal($Data);
+        return false;
+    }
+
+    /**
+     * @usage Write Events in the ics file
+     * @return void
+     */
+    public function writeEvent(){
+        if (file_exists($this->pathIcalSave.'/'.$this->name.'.ics')){
+            $file = fopen($this->pathIcalSave.'/'.$this->name.'.ics','a');
+            foreach ($this->Event as $Event) {
+                fwrite($file, "BEGIN:VEVENT\n");
+                fwrite($file,$Event->getDateStart()."\n");
+                fwrite($file, $Event->getDateEnd()."\n");
+                fwrite($file, $Event->getSummary()."\n");
+                fwrite($file, $Event->getUID()."\n");
+                fwrite($file, "END:VEVENT\n");
+            }
+            fwrite($file,"END:VCALENDAR");
+        }
+    }
+
     /*
      * Debug function
      */
@@ -84,8 +122,66 @@ class Ical
 }
 
 class EventIcal{
-    private $DateStart;
-    private $DateEnd;
-    private $Summary;
-    private $UID;
+    private string $DateStart;
+    private string $DateEnd;
+    private string $Summary;
+    private string $UID;
+
+    /**
+     * @param $Data
+     * @throws Exception
+     */
+    public function __construct($Data){
+        $this->DateStart = $Data['Begin'];
+        $this->DateEnd = $Data['End'];
+        $this->Summary = $Data['Summary'];
+        $this->UID = $Data['UID'];
+        $this->doEventIcal();
+    }
+
+    /**
+     * @usage Transform property of class as item for ics file
+     * @return void
+     * @throws Exception
+     */
+    private function doEventIcal():void{
+        $Begin = new DateTime($this->DateStart);
+        $End = new DateTime($this->DateEnd);
+        $this->DateStart = 'DTSTART;VALUE=DATE:'.$Begin->format('Ymd');
+        $this->DateEnd = 'DTEND;VALUE=DATE:'.$End->format('Ymd');
+        $this->Summary = 'SUMMARY:'.$this->Summary;
+        $this->UID = 'UID:'.$this->UID;
+    }
+
+    /*
+     * Get - Set
+     */
+
+    /**
+     * @return string - Date Begin as string
+     */
+    public function getDateStart():string {
+        return $this->DateStart;
+    }
+
+    /**
+     * @return string - Date End as string
+     */
+    public function getDateEnd():string {
+        return $this->DateEnd;
+    }
+
+    /**
+     * @return string - Summary as string
+     */
+    public function getSummary():string {
+        return $this->Summary;
+    }
+
+    /**
+     * @return string - UID as string
+     */
+    public function getUID():string {
+        return $this->UID;
+    }
 }
--- index.php
+++ index.php
@@ -12,6 +12,33 @@ $Ical = new Ical(array(
     'Name' => 'ReyzanCorp'
 ));
 
+$Event = array(
+    'Begin' =>  '2023-01-19',
+    'End' => '2023-01-20',
+    'Summary' => '01 | Event 1',
+    'UID' => 'Event_1@reyhome.com'
+);
+
+$Event2 = array(
+    'Begin' =>  '2023-01-20',
+    'End' => '2023-01-21',
+    'Summary' => '02 | Event 2',
+    'UID' => 'Event_1@reyhome.com'
+);
+
+$Event3 = array(
+    'Begin' =>  '2023-01-21',
+    'End' => '2023-01-22',
+    'Summary' => '03 | Event 3',
+    'UID' => 'Event_1@reyhome.com'
+);
+
+$Ical->addEvent($Event);
+$Ical->addEvent($Event2);
+$Ical->addEvent($Event3);
+
+$Ical->writeEvent();
+
 echo '<pre>';
 $Ical->debugViewIcal();
 echo '</pre>';
